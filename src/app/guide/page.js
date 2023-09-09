f57f5d5125d6747d91fdc062dea2f812b4b1e009
circleolami <circleolami@gmail.com>
2023-09-09 11:29:09 +0900

    guide

---
+++ src/app/guide/page.js
@@ -0,0 +1,32 @@
+"use client";
+
+import classes from "./guide.module.css";
+import { useEffect, useState } from "react";
+import { setAccount } from "@/stores/accountSlice";
+import { useSelector, useDispatch } from "react-redux";
+import Loading from "@/components/loading/page";
+
+export default function Join() {
+  const dispatch = useDispatch();
+  const accountName = useSelector((state) => state.account.name);
+
+  const [loading, setLoading] = useState(false);
+
+  const handleEnterName = (e) => {
+    dispatch(setAccount(e.target.value));
+  };
+
+  return (
+    <>
+      <div className={classes.guideContainer}>
+      <div className={classes.guideBox}>
+        <div className={classes.title}>Guide</div>
+        <br></br>
+        <div className={classes.inputContainer}>In Suzuran high school, the player has to increase the likability of one female NPC to 100 before dismissal. 
+        <br></br><br></br>
+        The conversation will take place during 6 breaks until 7th period, and each break is 3 minutes long. Keep in mind that you can't enter other classes during class time, and you can only have one person in a class!</div>
+    </div>
+      </div>
+    </>
+  );
+}
